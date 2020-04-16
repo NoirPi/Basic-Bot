67f3395465b4ef86d150a87e512e020d1263effe
NoirPi <34034616+NoirPi@users.noreply.github.com>
2020-04-16 20:30:00 +0200

    More Information on Bot start

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import locale
 import discord
+from discord import utils
 from discord.ext import commands
+import time
 
 
 # noinspection PyShadowingNames
@@ -9,6 +11,7 @@ def __init__(self, bot: commands.Bot):
 
 
 locale.setlocale(locale.LC_ALL, "en_US.utf8")
+start_time = time.time()
 bot = commands.Bot(command_prefix="!",
                    case_insensitive=True,
                    owner_id="")
@@ -16,17 +19,20 @@ bot = commands.Bot(command_prefix="!",
 
 @bot.event
 async def on_ready():
-    """Output after the Bot is fully loaded"""
+    """Output after the Bot fully loaded"""
+    end_time = time.time() - start_time
     await bot.change_presence(status=discord.Status.online)
     print(f'#-------------------------------#\n'
-          f'| Successfully logged in\n'
-          f'#-------------------------------#\n'
-          f'| Username:  {bot.user.name}\n'
-          f'| User ID:   {bot.user.id}\n'
+          f'| Username: {bot.user.name}\n'
+          f'| User ID: {bot.user.id}\n'
           f'| Developer: CodingSquad Team\n'
-          f'| OAuth URL: {discord.utils.oauth_url(bot.user.id)}\n'
+          f'| Guilds: {len(bot.guilds)}\n'
+          f'| Users: {len([member for member in bot.users if not member.bot])}\n'
+          f'| Base OAuth URL: {utils.oauth_url(bot.user.id)}\n'
+          f'| Bot started in \033[92m{"%.3f" % end_time}\033[0m seconds\n'
+          f'| Current Discord.py Version: {discord.__version__}\n'
           f'# ------------------------------#')
-    
+
 
 MODULES = [
     'commands',
