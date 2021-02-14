0a9fcc5f1e972ff928fb03559901a08378933502
xyzzzzzzzzzzzzzz <78847759+xyzzzzzzzzzzzzzz@users.noreply.github.com>
2021-02-14 13:39:53 +0000

    Changes on main.py
    Just made a few changes as well as updating it with the new discord.Intents.

--- main.py
+++ main.py
@@ -1,45 +1,38 @@
 import locale
-import time
+import datetime
 
 import discord
-from discord import utils
 from discord.ext import commands
 
 locale.setlocale(locale.LC_ALL, "en_US.utf8")
 start_time = time.time()
 
 
-class Bot(commands.AutoShardedBot):
-    def __init__(self):
-        super(Bot, self).__init__(
-            command_prefix="!",
-            case_insensitive=True, owner_id=[owner for owner in [1234567890]],
-            max_messages=5000)
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        """Output after the Bot fully loaded"""
-        end_time = time.time() - start_time
-        await self.change_presence(status=discord.Status.online)
-        print(f'#-------------------------------#\n'
-              f'| Username: {self.user.name}\n'
-              f'| User ID: {self.user.id}\n'
-              f'| Developer:  CodingSquad Team\n'
-              f'| Guilds: {len(self.guilds)}\n'
-              f'| Users: {len([member for member in self.users if not member.bot])}\n'
-              f'| Base OAuth URL: {utils.oauth_url(self.user.id)}\n'
-              f'| Bot started in \033[92m{"%.3f" % end_time}\033[0m seconds\n'
-              f'| Current Discord.py Version: {discord.__version__}\n'
-              f'# ------------------------------#')
-
-
-MODULES = [
-    'commands',
+modules = [
+    'commands'
 ]
 
-BasicBot = Bot()
 
-for module in MODULES:
-    BasicBot.load_extension(module)
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix='!', intents=discord.Intents.all(), case_insensitive=True)
+        
+        # loading extenstions
+        for module in modules:
+            try:
+                self.load_extension(module)
+            except Exception:
+                print(f'{module} not loaded.')
+    
+    async def on_ready():
+        """When the bot is ready, you don't need an event decorator."""
+        self.start_time = datetime.datetime.utcnow()
+        
+        # changing the bot status
+        await self.change_presence(status=discord.Status.online)
+        
+        print(f'Ready: {self.user} (ID: {self.user.id})')
 
-BasicBot.run("")
+
+basic_bot = Bot()
+basic_bot.run('token')
